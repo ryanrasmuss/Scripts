bab30a414657368d4a41f87a5929e2c678f25557
ryan <none@none.com>
2017-11-12 01:18:05 -0800

    updated mac_lookup script w/ update option and better flow

--- mac_lookup.sh
+++ mac_lookup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Simple mac lookup script
+# Author: Ryan Rasmuss
 # grep -i "2c-f0-ee" /var/lib/ieee-data/oui.txt
 # $1 mac_address_to_lookup
 
-if [ $# != 1 ] || [ $1 == '-h' ]; then
-    echo "usage: ./mac_lookup.sh mac_address"
+lookup=/var/lib/ieee-data/oui.txt
+
+if [ $# != 1 ] || [ $1 == '-h' ] || [ $1 == '--help' ] || [ $1 == '--h' ]; then
+    echo "usage: <./mac_lookup.sh> <mac_address>|<--help>|<--update>"
     echo "example: ./mac_lookup.sh 2c-f0-ee"
     echo "example: ./mac_lookup.sh 2cf0ee"
     exit
+elif [ $1 == '--update' ]; then
+    apt-get install ieee-data -y
+    exit
 else
-    grep -i "$1" /var/lib/ieee-data/oui.txt
+    grep -i "$1" $lookup
 fi
-
