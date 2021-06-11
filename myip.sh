968a7271aa98fe6ed2041439ca9352e49a41c2bc
Ryan Rasmuss <ryan.rasmuss@1touch.io>
2021-06-11 14:23:01 +0300

    new myip script

--- myip.sh
+++ myip.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
-server1="$(curl ipecho.net/plain)" 
-server2="$(curl icanhazip.com)"
+
+
+server1="$(curl -s ipecho.net/plain)" 
+server2="$(curl -s icanhazip.com)"
 
 echo $server1
 echo $server2
+
+country="$(curl -s https://ipapi.co/$server2/country/)"
+region="$(curl -s https://ipapi.co/$server2/region/)"
+
+#echo -e "\033[95m$country\033[0m"
+#echo -e "\033[95m$region\033[0m"
+
+
+get_country () 
+{
+   
+   case $country in
+
+       US)
+          echo -e "\033[95m$country\033[0m 🇺🇸"
+          ;;
+
+       UA)
+          echo -e "\033[1;93;44m$country\033[0m 🇺🇦"
+          ;;
+
+        
+       *)
+          echo -n "unknown"
+          ;;
+
+   esac
+
+}
+
+
+get_country
+echo -e "\033[95m$region\033[0m"
